3ba2ed70bcdece46499e69277f547d598a443772
sevonj <100710152+sevonj@users.noreply.github.com>
2025-05-05 18:05:07 +0300

    feat: pattern rendering

--- crates/nesmc-emu/src/nes_machine/ppu/mod.rs
+++ crates/nesmc-emu/src/nes_machine/ppu/mod.rs
@@ -111,62 +111,65 @@ impl Ppu {
         }
     }
 
+    // TODO: Cycle-accurate rendering
     fn fetch_nametable(&mut self, _bus: &mut Bus) {
-        //
+        // let current_px_idx = self.cycle - 1 + self.scanline * 256;
+        // let fetch_px_idx = (current_px_idx + 16) % self.framebuffer.len();
     }
 
+    // TODO: Cycle-accurate rendering
     fn fetch_attribute(&mut self, _bus: &mut Bus) {
         //
     }
 
+    // TODO: Cycle-accurate rendering
     fn fetch_pattern_lo(&mut self, _bus: &mut Bus) {
         self.pattern_tile_idx &= 0xff00;
         //let value = bus.read_ppu(address) as u16;
         //self.pattern_tile_idx |= value;
     }
 
+    // TODO: Cycle-accurate rendering
     fn fetch_pattern_hi(&mut self, _bus: &mut Bus) {
         self.pattern_tile_idx &= 0x00ff;
         //let value = bus.read_ppu(address + 1) as u16;
         //self.pattern_tile_idx |= value << 8;
     }
 
+    // TODO: Cycle-accurate rendering
     fn render_pixel(&mut self, bus: &mut Bus) {
+        let name_idx = (self.cycle as u16 - 1) / 8 + (self.scanline as u16 / 8) * 32;
+        let name_off = bus.ppu_regs.ctrl.base_nametable_addr + name_idx;
+        let tile_idx = bus.read_ppu(name_off) as u16;
+        let tile_addr = tile_idx * 0x10 + bus.ppu_regs.ctrl.base_bg_pattern_addr;
+
+        let x = (self.cycle as u16 - 1) % 8;
+        let y = self.scanline as u16 % 8;
+
+        let plane_0 = bus.read_ppu(tile_addr + y);
+        let plane_1 = bus.read_ppu(tile_addr + y + 8);
+
+        let val_lo = plane_0 << x & 0x80;
+        let val_hi = plane_1 << x & 0x80;
+
+        let color_idx = (val_lo >> 7) + (val_hi >> 6);
+
+        // Placeholder palette
+        let color = match color_idx {
+            0 => (0, 0, 0),
+            1 => (0x40, 0x40, 0x40),
+            2 => (0x80, 0x80, 0x80),
+            3 => (0xff, 0xff, 0xff),
+            _ => unreachable!(),
+        };
+
         let pixel_idx = self.cycle - 1 + self.scanline * 256;
         let pixel_off = pixel_idx * 3;
 
-        let name_idx = (self.cycle as u16 - 1) / 8 + (self.scanline as u16 / 8) * 32;
-        let name_off = bus.ppu_regs.ctrl.base_nametable_addr + name_idx;
-        let tile_idx = bus.read_ppu(name_off);
-        //println!("{tile_idx:02X}");
-
-        let color = tile_idx;
-        // Placeholder palette
-        //let color = match self.cycle % 4 {
-        //    // pattern[i] {
-        //    0 => 0x00,
-        //    1 => 0x0f,
-        //    2 => 0xf0,
-        //    3 => 0xff,
-        //    _ => unreachable!(),
-        //};
-
-        self.framebuffer[pixel_off] = color;
-        self.framebuffer[pixel_off + 1] = color;
-        self.framebuffer[pixel_off + 2] = color;
+        self.framebuffer[pixel_off] = color.0;
+        self.framebuffer[pixel_off + 1] = color.1;
+        self.framebuffer[pixel_off + 2] = color.2;
     }
-
-    //fn read_pattern(&self, table: u8, index: u8, slice: u8) -> [u8; 8] {
-    //todo!()
-    //let address = self.pattern_tile_idx;
-    //let lo = self.read(address);
-    //let hi = self.read(address + 8);
-    //let mut pattern = [0u8; 8];
-    //for x in 0..8 {
-    //    pattern[7 - x] = ((lo >> x) & 1) | (((hi >> x) << 1) & 2);
-    //}
-    //pattern
-    //}
 
     pub fn framebuffer(&self) -> &[u8; 256 * 240 * 3] {
         &self.framebuffer
